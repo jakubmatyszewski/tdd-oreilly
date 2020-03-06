39cfb64da4267931d389e5a24a05dfa51b0a684e
jakubmatyszewski <jakubmatyszewski1@gmail.com>
2020-03-06 16:58:53 +0100

    Refactor with @wait decorator

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -25,7 +25,7 @@ class MyListsTest(FunctionalTest):
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         email = 'jakub@example.com'
-        self.browser.get(self.live_server_url)
+        browser = self.browser.get(self.live_server_url)
         self.wait_to_be_logged_out(email)
 
         # Edith is a logged-in user
