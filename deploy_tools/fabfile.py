3dae2fb56888a3a61d1742b39dfdf56f1aea3d06
jakubmatyszewski <jakubmatyszewski1@gmail.com>
2020-03-08 20:28:46 +0100

    Fix session tools; update fabfile for email pass

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -38,8 +38,9 @@ def _create_or_update_dotenv():
     if 'DJANGO_SECRET_KEY' not in current_contents:
         new_secret = ''.join(random.SystemRandom().choices(
             'abcdefghijklmnopqrstuvwxyz0123456789', k=50
-            ))
-        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+        ))
+    email_password = os.environ['EMAIL_PASSWORD']
+    append('.env', f'EMAIL_PASSWORD={email_password}')
 
 
 def _update_static_files():
