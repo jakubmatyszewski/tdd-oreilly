47b65e2d03916282c195189fdc37610b1046e443
jakubmatyszewski <jakubmatyszewski1@gmail.com>
2020-03-08 16:26:37 +0100

    Adjust session settings to work on server

--- superlists/settings.py
+++ superlists/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'lists',
     'accounts',
+    'functional_tests',
 ]
 
 AUTH_USER_MODEL = 'accounts.User'
@@ -135,3 +136,20 @@ EMAIL_HOST_USER = 'auto@jakubmatyszewski.site'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
